bcbee079652405836f186f4476de4306d869f2b2
Vladimir Atanov <atanov.v@gmail.com>
2019-01-16 17:34:02 +0200

    Create week_02_04_task_1.py

---
+++ Lesson01/week_02_04_task_1.py
@@ -0,0 +1,20 @@
+answers = {'hello': "Hi!", 'how are you': 'Fine', 'bye': 'See you'}
+
+def get_answer(question):
+    return answers.get(question.lower(), 'Question is not found.')
+
+
+def main():
+    try:
+        while True:
+            question = input ('Enter question: ')
+            if question.lower() != 'exit':
+                print(get_answer(question))
+            else:
+                break
+    except(KeyboardInterrupt):
+        print('Program is closing... Bye!')
+
+
+if __name__ == '__main__':
+    main()
