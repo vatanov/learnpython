25cec3b7a08a2e5bcb50a3e7d4579be978999c0b
Vladimir Atanov <atanov.v@gmail.com>
2019-01-16 16:12:03 +0200

    Update week_02_02_task_2.py

--- Lesson01/week_02_02_task_2.py
+++ Lesson01/week_02_02_task_2.py
@@ -1,7 +1,34 @@
+scores = [{'school_class': '1b',
+           'scores': [4,2,5]
+           },
+          {'school_class': '5a',
+           'scores': [5,2,5,4]
+           },
+          {'school_class': '11v',
+           'scores': [3,3,2]
+           }]
+
+def get_class_average(scores, class_name):
+    for i in scores:
+        if i['school_class'] == class_name:
+            return sum(i['scores']) / i['scores'].__len__()
+
+
+def get_school_average(scores):
+    school_average = []
+    for i in scores:
+        school_average.append(sum(i['scores']) / i['scores'].__len__())
+
+    return sum(school_average) / school_average.__len__()
+
+
 def main():
-    my_string = input('Enter a string: ')
-    for i in my_string:
-        print(i)
+    print('1-B average score is:', get_class_average(scores, '1b'))
+    print('5-A average score is:', get_class_average(scores, '5a'))
+    print('11-V average score is:', get_class_average(scores, '11v'))
+
+    print('Whole school average score is:', get_school_average(scores))
+
 
 if __name__ == '__main__':
     main()
